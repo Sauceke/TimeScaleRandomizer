e7e292d6df57fbb6070d071cdc5c2163da9107af
Sauceke <saucekedesu@protonmail.com>
2023-09-19 20:56:14 +0300

    Check in

---
+++ TimeScaleRandomizer/ConfigFileRangeExtension.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx.Configuration;
+using UnityEngine;
+
+namespace TimeScaleRandomizer
+{
+    public static class ConfigFileRangeExtension
+    {
+        public static void BindRange(this ConfigFile configFile, string section, string key,
+            float lowerDefaultValue, float upperDefaultValue, string description,
+            AcceptableValueRange<float> acceptableValues,
+            out ConfigEntry<float> lower, out ConfigEntry<float> upper, string format = "F2")
+        {
+            var lowerEntry = configFile.Bind(
+                section: section,
+                key: key + " (Min)",
+                defaultValue: lowerDefaultValue,
+                configDescription: new ConfigDescription(
+                    description: "Minimum value of: " + description,
+                    acceptableValues: acceptableValues,
+                    tags: new ConfigurationManagerAttributes { Browsable = false }));
+            var upperEntry = configFile.Bind(
+                section: section,
+                key: key + " (Max)",
+                defaultValue: upperDefaultValue,
+                configDescription: new ConfigDescription(
+                    description: "Maximum value of: " + description,
+                    acceptableValues: acceptableValues,
+                    tags: new ConfigurationManagerAttributes { Browsable = false }));
+            void Drawer(ConfigEntryBase entry) =>
+                RangeDrawer(lowerEntry, upperEntry, acceptableValues, format);
+            configFile.Bind(
+                section: section,
+                key: key,
+                defaultValue: "Ignore this",
+                configDescription: new ConfigDescription(
+                    description: description,
+                    tags: new ConfigurationManagerAttributes
+                    {
+                        CustomDrawer = Drawer,
+                        HideDefaultButton = true
+                    }));
+            lower = lowerEntry;
+            upper = upperEntry;
+        }
+        
+        private static void RangeDrawer(ConfigEntry<float> lowerEntry,
+            ConfigEntry<float> upperEntry, AcceptableValueRange<float> acceptableValues,
+            string format)
+        {
+            // Allocate a fixed space to the value labels so the slider won't get displaced while
+            // being used
+            var minLabel = new GUIContent(acceptableValues.MinValue.ToString(format));
+            var maxLabel = new GUIContent(acceptableValues.MaxValue.ToString(format));
+            float labelWidth = Mathf.Max(GUI.skin.label.CalcSize(minLabel).x,
+                GUI.skin.label.CalcSize(maxLabel).x);
+            float lower = lowerEntry.Value;
+            float upper = upperEntry.Value;
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label(lower.ToString(format), GUILayout.Width(labelWidth));
+                RangeSlider.Create(ref lower, ref upper,
+                    acceptableValues.MinValue, acceptableValues.MaxValue);
+                GUILayout.Label(upper.ToString(format), GUILayout.Width(labelWidth));
+                if (GUILayout.Button("Reset", GUILayout.ExpandWidth(false)))
+                {
+                    lower = (float)lowerEntry.DefaultValue;
+                    upper = (float)upperEntry.DefaultValue;
+                }
+            }
+            GUILayout.EndHorizontal();
+            lowerEntry.Value = lower;
+            upperEntry.Value = upper;
+        }
+    }
+}
---
+++ TimeScaleRandomizer/Plugin.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace TimeScaleRandomizer
+{
+    [BepInPlugin("Sauceke.TimeScaleRandomizer", "Time Scale Randomizer", "1.0.0")]
+    internal class Plugin : BaseUnityPlugin
+    {
+        private ConfigEntry<bool> enabled;
+        private ConfigEntry<float> timeScaleMin;
+        private ConfigEntry<float> timeScaleMax;
+        private ConfigEntry<float> waitSecsMin;
+        private ConfigEntry<float> waitSecsMax;
+        private ConfigEntry<float> changeSecsMin;
+        private ConfigEntry<float> changeSecsMax;
+
+        private void Start()
+        {
+            const string title = "Randomization Settings";
+            enabled = Config.Bind(
+                section: title,
+                key: "Enabled",
+                defaultValue: true,
+                configDescription: new ConfigDescription(
+                    description: "Whether to enable time scale randomization.",
+                    tags: new ConfigurationManagerAttributes { Order = 1000 }));
+            Config.BindRange(
+                section: title,
+                key: "Time Scale Range",
+                lowerDefaultValue: 0.75f,
+                upperDefaultValue: 1.25f,
+                description: "Range of speeds to select from.",
+                acceptableValues: new AcceptableValueRange<float>(1 / 3f, 3f),
+                lower: out timeScaleMin,
+                upper: out timeScaleMax,
+                format: "P0");
+            Config.BindRange(
+                section: title,
+                key: "Waiting Time Range (seconds)",
+                lowerDefaultValue: 5f,
+                upperDefaultValue: 15f,
+                description: "Range of how much to wait before changing the time scale.",
+                acceptableValues: new AcceptableValueRange<float>(0f, 30f),
+                lower: out waitSecsMin,
+                upper: out waitSecsMax);
+            Config.BindRange(
+                section: title,
+                key: "Change Time Range (seconds)",
+                lowerDefaultValue: 1f,
+                upperDefaultValue: 5f,
+                description: "Range of how long it will take to gradually change the time scale.",
+                acceptableValues: new AcceptableValueRange<float>(0f, 30f),
+                lower: out changeSecsMin,
+                upper: out changeSecsMax);
+            StartCoroutine(Run());
+        }
+        
+        private IEnumerator Run()
+        {
+            while (true)
+            {
+                if (!enabled.Value)
+                {
+                    Time.timeScale = 1f;
+                    yield return new WaitForSeconds(1f);
+                    continue;
+                }
+                if (Time.timeScale == 0f)
+                {
+                    yield return new WaitForSeconds(1f);
+                    continue;
+                }
+                float waitSecs = Random.Range(waitSecsMin.Value, waitSecsMax.Value);
+                yield return new WaitForSeconds(waitSecs * Time.timeScale);
+                float newTimeScale = Random.Range(timeScaleMin.Value, timeScaleMax.Value);
+                float changeSecs = Random.Range(changeSecsMin.Value, changeSecsMax.Value);
+                yield return ChangeTimeScale(newTimeScale, changeSecs);
+            }
+        }
+
+        private static IEnumerator ChangeTimeScale(float targetTimeScale, float durationSecs)
+        {
+            float startTimeScale = Time.timeScale;
+            const float refreshTimeSecs = 0.1f;
+            int iterations = Mathf.RoundToInt(durationSecs / refreshTimeSecs);
+            for (int i = 0; i < iterations; i++)
+            {
+                yield return new WaitForSeconds(refreshTimeSecs * Time.timeScale);
+                Time.timeScale = Mathf.Lerp(startTimeScale, targetTimeScale, (i + 1f) / iterations);
+            }
+        }
+    }
+}
